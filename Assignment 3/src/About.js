ed4b316eba1c6d33e6d1fa8e8616f5f38a64d960
Mindrigan Dumitru <51724570+dumitru1111@users.noreply.github.com>
2021-06-20 01:10:00 -0400

    Add files via upload

---
+++ Assignment 3/src/About.js
@@ -0,0 +1,28 @@
+import {Button, Card} from 'react-bootstrap'
+import { useHistory } from "react-router-dom"; 
+
+
+export default function About(){
+
+    let history = useHistory();
+      
+    return( 
+        <>
+          <Card style={{ width: '50rem' }}>
+              <Card.Body>
+                    <Card.Title>About</Card.Title>
+                    <Card.Text>
+                    This project was created in React. There are 3 main components
+                    <ul>
+                        <li>Restaurants</li>
+                        <li>Restaurant</li>
+                        <li>About</li>
+                        <li>Not Found</li>
+                    </ul>
+                    </Card.Text>
+                </Card.Body>
+                <Button  variant="primary" onClick={() => { history.push("/Restaurants")}}> Back to Restaurants</Button>
+            </Card>
+        </>
+    );   
+}
---
+++ Assignment 3/src/ComponentName.js
@@ -0,0 +1,7 @@
+<p>Restaurants query: query</p>
+
+<p>Restaurant id: id</p>
+
+<p>About</p>
+
+<div><h1>Not Found</h1><p></p>
